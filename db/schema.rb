142c40d5d8290f33457ecee9fc51d0be9c48b53a
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-13 00:11:33 +0530

    Addition of user login with tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_103017) do
+ActiveRecord::Schema.define(version: 2018_12_12_130911) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.integer "number_of_tweets"
     t.string "email"
     t.string "firstname"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,40 +12,40 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'validity_of_user_with_invalid_email' do
-  	user = User.new(:username => "scube", :email => "aaa@.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube", :email => "aaa@.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert_not user.valid?
 
-  	user = User.new(:username => "scube2", :email => "aaa.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube2", :email => "aaa.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert_not user.valid?
 
-  	user = User.new(:username => "scube3", :email => "aaacom", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube3", :email => "aaacom", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert_not user.valid?
 
-  	user = User.new(:username => "scube4", :email => "aaa@..gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube4", :email => "aaa@..gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert_not user.valid?
   end
 
   test 'uniqueness_of_user' do
-  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert user.valid?
   	user.save
-  	user1 = User.new(:username => "scube", :email => "aaakjhjk@gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user1 = User.new(:username => "scube", :email => "aaakjhjk@gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
  	assert_not user1.valid?
  	user.destroy
   end
 
   test 'uniqueness_of_email' do
-  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert user.valid?
   	user.save
 
-  	user1 = User.new(:username => "scube97", :email => "aaa@gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user1 = User.new(:username => "scube97", :email => "aaa@gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert_not user1.valid?
   	user.destroy
   end
 
   test 'perfect_object_gets_created' do
-  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :password => "shubham", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
+  	user = User.new(:username => "scube", :email => "aaa@gmail.com", :firstname => "Shubham", :date_of_birth => "06/08/1997", :date_of_joining => "06/08/1997")
   	assert user.valid?
   end
 end
