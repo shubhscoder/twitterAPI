b18208cf834b85c5cbdf607a5400dd0de4b304e1
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-13 16:35:35 +0530

    Login working

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -17,7 +17,6 @@ class AuthenticateUser
 	def user
 		user = User.find_by_email(email)
 		return user if user and user.authenticate(password)
-
 		errors.add :user_authentication, 'invalid credentials'
 		nil 
 	end
