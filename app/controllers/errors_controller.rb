b4b5d646d601c394e28df8b3683bffcbdce4f841
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-15 11:07:59 +0530

    Proper spacing

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
 	def not_found
-		render :json => { status: "Nok", message: "Request resource dose not exist"}
+		render :json => { status: "Nok", message: "Request resource dose not exist" }
 	end
 
 	def exception
-		render :json => { status: "Nok", message: "System error. Please try again"}
+		render :json => { status: "Nok", message: "System error. Please try again" }
 	end
 end
